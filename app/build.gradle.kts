1a7b1b0d8fb1b077a80f1fcdebcad052f2ffe3cb
Egito Filho <egitofilho4@gmail.com>
2024-12-07 11:01:50 -0300

    DESAFIO_GERADOR_DE_NUMEROS_PARTE_02

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.dev.bernardoslailati.sorteadordenumeros"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.dev.bernardoslailati.sorteadordenumeros"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
